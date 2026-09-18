4dea76fadab76da296064d19e7ea6925c487ba6a
Alex Dempster <alex.dempster@signinsolutions.com>
2026-09-18 11:32:54 +0100

    build: bump example compile sdk version

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "my.alexl.multicamera_example"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
